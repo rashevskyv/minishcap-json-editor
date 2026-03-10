20ff85940dbe2d273afd100722829c37a21636c3
rashevskyv <rashevsky.v@gmail.com>
2026-03-10 14:36:21 +0200

    chore: cleanup test files and update pokemon config

--- test_load.py
+++
@@ -1,7 +0,0 @@
-
-import json
-with open('plugins/pokemon_fr/font_map.json', 'r', encoding='utf-8') as f:
-    d = json.load(f)
-with open('test_load.txt', 'w') as f:
-    f.write(str(len(d)))
-
--- test_width.py
+++
@@ -1,7 +0,0 @@
-
-from utils.utils import calculate_string_width
-font_map = {'{PLAYER}': {'width': 56}}
-res = calculate_string_width('{PLAYER} test', font_map)
-with open('test_res.txt', 'w') as f:
-    f.write('Width: ' + str(res))
-
